01b543f1a3fd374c5b83907edab2e8f557886e60
Vanderlucio Lopes <vanderlucio.lopes@bol.com.br>
2026-08-15 18:25:11 -0300

    chore: limpar pastas versao_producao e versao_nodered

--- versao_producao/start_service.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Fanuc Industrial Server - Producao
-cd /d "%~dp0"
-echo =============================================================
-echo   INICIANDO SERVIDOR FANUC FOCAS / CLP (VERSAO PRODUCAO)
-echo =============================================================
-node src/server.js
-pause
